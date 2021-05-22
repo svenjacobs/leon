f33a01e632b62492bfa4a77b180f461cd94debba
Sven Jacobs <github@svenjacobs.com>
2021-05-22 16:02:09 +0200

    Use current branch for version code calculation

--- buildSrc/src/main/java/com/svenjacobs/app/leon/buildSrc/VersionCode.kt
+++ buildSrc/src/main/java/com/svenjacobs/app/leon/buildSrc/VersionCode.kt
@@ -1,7 +1,8 @@
 package com.svenjacobs.app.leon.buildSrc
 
 val versionCode by lazy {
-    val commits = execute("git", "rev-list", "--count", "origin/main")
+    val currentBranch = execute("git", "branch", "--show-current")
+    val commits = execute("git", "rev-list", "--count", "origin/$currentBranch")
     if (commits.isBlank()) throw IllegalArgumentException("Could not determine commit count")
     commits.toInt()
 }
