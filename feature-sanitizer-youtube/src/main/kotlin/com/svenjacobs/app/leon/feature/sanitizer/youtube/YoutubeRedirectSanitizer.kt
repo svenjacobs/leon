4860702ffb8ab28c7f6c85d78ed407c3399aacbb
Sven Jacobs <github@svenjacobs.com>
2022-09-17 20:44:52 +0200

    feat: Add YouTube redirect sanitizer

--- feature-sanitizer-youtube/src/main/kotlin/com/svenjacobs/app/leon/feature/sanitizer/youtube/YoutubeRedirectSanitizer.kt
+++ feature-sanitizer-youtube/src/main/kotlin/com/svenjacobs/app/leon/feature/sanitizer/youtube/YoutubeRedirectSanitizer.kt
@@ -16,25 +16,11 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-rootProject.name = "Leon"
-include(
-	":core-common",
-	":core-domain",
-	":feature-sanitizer-amazon",
-	":feature-sanitizer-amazon-smile",
-	":feature-sanitizer-aol-search",
-	":feature-sanitizer-empty-parameters",
-	":feature-sanitizer-facebook",
-	":feature-sanitizer-flipkart",
-	":feature-sanitizer-google-analytics",
-	":feature-sanitizer-google-search",
-	":feature-sanitizer-instagram",
-	":feature-sanitizer-netflix",
-	":feature-sanitizer-session-ids",
-	":feature-sanitizer-spotify",
-	":feature-sanitizer-twitter",
-	":feature-sanitizer-webtrekk",
-	":feature-sanitizer-yahoo-search",
-	":feature-sanitizer-youtube",
-	":app",
+package com.svenjacobs.app.leon.feature.sanitizer.youtube
+
+import com.svenjacobs.app.leon.core.domain.sanitizer.SearchResultSanitizer
+import javax.inject.Inject
+
+class YoutubeRedirectSanitizer @Inject constructor() : SearchResultSanitizer(
+	Regex("[?&]q=([^&]+)"),
 )
