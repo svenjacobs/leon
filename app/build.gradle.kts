f36836279202da56aac611fb636dc12391593fe8
Sven Jacobs <github@svenjacobs.com>
2023-08-22 11:50:48 +0200

    chore: Update dependencies (#218)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,12 +74,12 @@ android {
 	}
 
 	compileOptions {
-		sourceCompatibility = JavaVersion.VERSION_11
-		targetCompatibility = JavaVersion.VERSION_11
+		sourceCompatibility = JavaVersion.VERSION_17
+		targetCompatibility = JavaVersion.VERSION_17
 	}
 
 	kotlinOptions {
-		jvmTarget = "11"
+		jvmTarget = "17"
 		freeCompilerArgs = listOf(
 			"-opt-in=kotlinx.coroutines.ExperimentalCoroutinesApi",
 			"-opt-in=kotlin.RequiresOptIn",
