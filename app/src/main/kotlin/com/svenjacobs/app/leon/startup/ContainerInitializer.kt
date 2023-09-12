6f207966c56eb43772f59fc029be345c48aa7232
Sven Jacobs <github@svenjacobs.com>
2023-09-12 08:57:41 +0200

    feat: Add sanitizer for AT Analytics (#242)

--- app/src/main/kotlin/com/svenjacobs/app/leon/startup/ContainerInitializer.kt
+++ app/src/main/kotlin/com/svenjacobs/app/leon/startup/ContainerInitializer.kt
@@ -24,6 +24,7 @@ import com.svenjacobs.app.leon.core.domain.sanitizer.aliexpress.AliexpressSaniti
 import com.svenjacobs.app.leon.core.domain.sanitizer.amazon.AmazonProductSanitizer
 import com.svenjacobs.app.leon.core.domain.sanitizer.amazon.AmazonSanitizer
 import com.svenjacobs.app.leon.core.domain.sanitizer.aol.AolSearchSanitizer
+import com.svenjacobs.app.leon.core.domain.sanitizer.at.AtAnalyticsSanitizer
 import com.svenjacobs.app.leon.core.domain.sanitizer.change.ChangeSanitizer
 import com.svenjacobs.app.leon.core.domain.sanitizer.cx.CxAnalyticsSanitizer
 import com.svenjacobs.app.leon.core.domain.sanitizer.ebay.EbaySanitizer
@@ -72,6 +73,7 @@ class ContainerInitializer : DistinctInitializer<Unit> {
 				AmazonProductSanitizer(),
 				AmazonSanitizer(),
 				AolSearchSanitizer(),
+				AtAnalyticsSanitizer(),
 				ChangeSanitizer(),
 				CxAnalyticsSanitizer(),
 				EbaySanitizer(),
