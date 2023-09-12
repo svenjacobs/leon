4c3d1487ae96f37cce415d3df669ab98d0883d9f
Sven Jacobs <github@svenjacobs.com>
2023-09-12 08:18:09 +0200

    feat: Add sanitizer for AT Analytics

--- app/src/main/kotlin/com/svenjacobs/app/leon/startup/ContainerInitializer.kt
+++ app/src/main/kotlin/com/svenjacobs/app/leon/startup/ContainerInitializer.kt
@@ -24,6 +24,7 @@ import com.svenjacobs.app.leon.core.domain.sanitizer.aliexpress.AliexpressSaniti
 import com.svenjacobs.app.leon.core.domain.sanitizer.amazon.AmazonProductSanitizer
 import com.svenjacobs.app.leon.core.domain.sanitizer.amazon.AmazonSanitizer
 import com.svenjacobs.app.leon.core.domain.sanitizer.aol.AolSearchSanitizer
+import com.svenjacobs.app.leon.core.domain.sanitizer.at.AtAnalyticsSanitizer
 import com.svenjacobs.app.leon.core.domain.sanitizer.change.ChangeSanitizer
 import com.svenjacobs.app.leon.core.domain.sanitizer.cx.CxAnalyticsSanitizer
 import com.svenjacobs.app.leon.core.domain.sanitizer.ebay.EbaySanitizer
@@ -71,6 +72,7 @@ class ContainerInitializer : DistinctInitializer<Unit> {
 				AmazonProductSanitizer(),
 				AmazonSanitizer(),
 				AolSearchSanitizer(),
+				AtAnalyticsSanitizer(),
 				ChangeSanitizer(),
 				CxAnalyticsSanitizer(),
 				EbaySanitizer(),
---
+++ core-domain/src/main/kotlin/com/svenjacobs/app/leon/core/domain/sanitizer/at/AtAnalyticsSanitizer.kt
@@ -0,0 +1,37 @@
+/*
+ * Léon - The URL Cleaner
+ * Copyright (C) 2023 Sven Jacobs
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.svenjacobs.app.leon.core.domain.sanitizer.at
+
+import android.content.Context
+import com.svenjacobs.app.leon.core.common.regex.RegexFactory
+import com.svenjacobs.app.leon.core.domain.R
+import com.svenjacobs.app.leon.core.domain.sanitizer.RegexSanitizer
+import com.svenjacobs.app.leon.core.domain.sanitizer.Sanitizer
+import com.svenjacobs.app.leon.core.domain.sanitizer.SanitizerId
+
+class AtAnalyticsSanitizer : RegexSanitizer(
+	regex = RegexFactory.ofWildcardParameter("at_"),
+) {
+
+	override val id = SanitizerId("at_analytics")
+
+	override fun getMetadata(context: Context) = Sanitizer.Metadata(
+		name = context.getString(R.string.sanitizer_at_analytics_name),
+	)
+}
