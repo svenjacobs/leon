0cd101db3922229ffa7ffcb5ed423a651435469e
svenjacobs <svenjacobs@users.noreply.github.com>
2023-09-14 06:54:21 +0000

    [skip ci] Increment version code & name for next release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
 		applicationId = "com.svenjacobs.app.leon"
 		minSdk = Android.minSdk
 		targetSdk = Android.targetSdk
-		versionCode = 255
-		versionName = "34"
+		versionCode = 256
+		versionName = "35"
 
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 	}
