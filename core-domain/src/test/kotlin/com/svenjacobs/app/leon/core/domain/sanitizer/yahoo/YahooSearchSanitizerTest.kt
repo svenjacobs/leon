ce0776ba8562167838be40340b7b78e63933456a
Sven Jacobs <github@svenjacobs.com>
2023-10-04 08:48:29 +0200

    fix: Yahoo sanitizer for search query links (#289)

--- core-domain/src/test/kotlin/com/svenjacobs/app/leon/core/domain/sanitizer/yahoo/YahooSearchSanitizerTest.kt
+++ core-domain/src/test/kotlin/com/svenjacobs/app/leon/core/domain/sanitizer/yahoo/YahooSearchSanitizerTest.kt
@@ -1,6 +1,6 @@
 /*
  * Léon - The URL Cleaner
- * Copyright (C) 2022 Sven Jacobs
+ * Copyright (C) 2023 Sven Jacobs
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -23,19 +23,30 @@ import io.kotest.matchers.shouldBe
 
 class YahooSearchSanitizerTest : WordSpec(
 	{
+		val sanitizer = YahooSearchSanitizer()
 
 		"invoke" should {
 
-			"extract URL from Yahoo search link" {
-				val sanitizer = YahooSearchSanitizer()
-
-				val result = sanitizer(
+			"extract URL from Yahoo search result link" {
+				sanitizer(
 					"https://r.search.yahoo.com/_ylt=A0geKLovoVtisIEAUapx.9w4;_ylu=Y29sbwNi" +
 						"ZjEEcG9zAzQEdnRpZAMEc2VjA3Ny/RV=2/RE=1650201007/RO=10/RU=https%3a%2f%2fg" +
 						"ithub.com%2fsvenjacobs%2fleon/RK=2/RS=rHoItccMzwyZAXsJuDMkBaKUMx0-",
-				)
+				) shouldBe "https://github.com/svenjacobs/leon"
+			}
 
-				result shouldBe "https://github.com/svenjacobs/leon"
+			"clean Yahoo search query link" {
+				sanitizer(
+					"https://search.yahoo.com/search?p=hi&fr=yfp-hrmob&fr2=p%3Afp%2Cm%3Asb&" +
+						".tsrc=yfp-hrmob&ei=UTF-8&fp=1&toggle=1&cop=mss",
+				) shouldBe "https://search.yahoo.com/search?p=hi"
+			}
+		}
+
+		"matchesDomain" should {
+
+			"match search.yahoo.com" {
+				sanitizer.matchesDomain("https://search.yahoo.com") shouldBe true
 			}
 		}
 	},
